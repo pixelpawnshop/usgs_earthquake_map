4a5aff57b8778c25f29fe908c19930317e953166
pixelpawnshop <134876395+pixelpawnshop@users.noreply.github.com>
2024-09-01 04:47:46 +0200

    forcee

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css'; // Import global styles (if any)
 import App from './App'; // Import the App component
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
